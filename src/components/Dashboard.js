e2ab756de8245a7002548afd5a934f0523611066
xuyang0602 <wang.xuyang0602@gmail.com>
2023-11-08 09:32:50 -0800

    Add Basic Components

---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ProjectItem from './Project/ProjectItem';
+
+export default class Dashboard extends Component {
+  render() {
+    return (
+      <>
+        <div>Dashboard</div>
+        <ProjectItem />
+      </>
+    );
+  }
+}
