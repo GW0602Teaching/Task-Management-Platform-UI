c303b44fc52edcaf24dee8b51989fba6e7e22942
xuyang0602 <wang.xuyang0602@gmail.com>
2024-01-16 16:54:18 -0800

    Fix bug

--- src/actions/projectActions.js
+++ src/actions/projectActions.js
@@ -11,7 +11,7 @@ export const createProject = (project) => async (dispatch) => {
     console.log('error', err);
     dispatch({
       type: GET_ERRORS,
-      payload: err?.response?.data,
+      payload: err?.response?.data || err?.config?.data,
     });
   }
 };
