b77cbc9385785391456e5c10bce98f77d9c15464
xuyang0602 <wang.xuyang0602@gmail.com>
2024-02-06 13:56:21 -0800

    User login

--- src/actions/securityActions.js
+++ src/actions/securityActions.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-import { GET_ERRORS } from './types';
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
+import setJWTToken from '../securityUtils/setJWTToken';
+import { jwtDecode } from 'jwt-decode';
 
 export const createNewUser =
   (newUser, callbackFuc) => async (dispatch) => {
@@ -17,5 +19,34 @@ export const createNewUser =
         );
     } catch (err) {
       console.log('error', err);
+    }
+  };
+
+export const login =
+  (LoginRequest, callbackFuc) => async (dispatch) => {
+    try {
+      await axios
+        .post('http://localhost:8080/api/users/login', LoginRequest)
+        .then((res) => {
+          console.log(res);
+          const { token } = res.data;
+          localStorage.setItem('jwtToken', token);
+          setJWTToken(token);
+          const decodedRes = jwtDecode(token);
+          dispatch({
+            type: SET_CURRENT_USER,
+            payload: decodedRes,
+          });
+          callbackFuc('/dashboard');
+        })
+        .catch((err) => {
+          console.log(err);
+          dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data,
+          });
+        });
+    } catch (err) {
+      console.error(err);
     }
   };
--- src/components/UserManagement/Login.js
+++ src/components/UserManagement/Login.js
@@ -1,41 +1,91 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import classnames from 'classnames';
+import { login } from '../../actions/securityActions';
+import { useNavigate } from 'react-router-dom';
 
-class Login extends Component {
-  render() {
-    return (
-      <div className="login">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-8 m-auto">
-              <h1 className="display-4 text-center">Log In</h1>
-              <form action="dashboard.html">
-                <div className="m-3">
-                  <input
-                    type="email"
-                    className="form-control form-control-lg"
-                    placeholder="Email Address"
-                    name="email"
-                  />
-                </div>
-                <div className="m-3">
-                  <input
-                    type="password"
-                    className="form-control form-control-lg"
-                    placeholder="Password"
-                    name="password"
-                  />
-                </div>
+const Login = (props) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigator = useNavigate();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const request = {
+      username,
+      password,
+    };
+
+    const { login } = props;
+
+    login(request, navigator);
+  };
+
+  const onChange = (e) => {
+    const val = e.target.value;
+    switch (e.target.name) {
+      case 'username':
+        setUsername(val);
+        break;
+      case 'password':
+        setPassword(val);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const { errors } = props;
+
+  return (
+    <div className="login">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 m-auto">
+            <h1 className="display-4 text-center">Log In</h1>
+            <form onSubmit={onSubmit}>
+              <div className="m-3">
                 <input
-                  type="submit"
-                  className="btn btn-info btn-block mt-4"
+                  type="text"
+                  className="form-control form-control-lg"
+                  placeholder="Email Address"
+                  name="username"
+                  value={username}
+                  onChange={onChange}
                 />
-              </form>
-            </div>
+              </div>
+              <div className="m-3">
+                <input
+                  type="password"
+                  className="form-control form-control-lg"
+                  placeholder="Password"
+                  name="password"
+                  value={password}
+                  onChange={onChange}
+                />
+              </div>
+              <input
+                type="submit"
+                className="btn btn-info btn-block mt-4"
+              />
+            </form>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-export default Login;
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  security: state.security,
+  errors: state.errors,
+});
+
+export default connect(mapStateToProps, { login })(Login);
